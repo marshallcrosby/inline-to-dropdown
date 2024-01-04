421975c207afc517e387ead6ed4c4f10c2e9a95b
Marshall Crosby <marshall@blendinteractive.com>
2024-01-04 09:35:36 -0600

    Performance and bug fixes. Now uses :has() pseudo-class

--- dist/js/inline-to-dropdown.js
+++ dist/js/inline-to-dropdown.js
@@ -1,8 +1,8 @@
 /*!
-    * Inline to dropdown v1.0.1
+    * Inline to dropdown v2.0.0
     * Automatically add/remove overflown inline items into a dropdown.
     *
-    * Copyright 2023 Marshall Crosby
+    * Copyright 2024 Marshall Crosby
     * https://marshallcrosby.com
 */
 
@@ -10,20 +10,14 @@
     const inlineToDropdownEl = document.querySelectorAll('.inline-to-dropdown');
 
     if (inlineToDropdownEl.length) {
-        let container;
-        let dropdown;
-        let dropdownChildren;
-        let dropdownChildItem;
-        let lastDropdownChildItem;
-
         inlineToDropdownEl.forEach(item => {
             reflowItems(item);
             item.classList.add('js-inline-to-dropdown--loaded')
         });
 
         const windowElement = document.documentElement;
-        
         let windowWidth = window.innerWidth;
+        
         window.addEventListener('resize', () => {
             let newWindowWidth = window.innerWidth;
             if (windowWidth !== newWindowWidth) {
@@ -33,7 +27,7 @@
         });
         
         let windowWidthDebounce = window.innerWidth;
-        window.addEventListener('resize', debounce(() => {
+        window.addEventListener('resize', itdDebounce(() => {
             let newWindowWidthDebounce = window.innerWidth;
             if (windowWidthDebounce !== newWindowWidthDebounce) {
                 inlineToDropdownEl.forEach(item => {
@@ -45,36 +39,28 @@
         }));
         
         function reflowItems(el) {
-            el.classList.add('js-dropdown-has-children');
-            container = el.querySelector('.inline-to-dropdown__group');
-            dropdown = el.querySelector('.inline-to-dropdown__menu');
-            dropdownChildren = dropdown.querySelectorAll('.inline-to-dropdown__item');
+            const container = el.querySelector('.inline-to-dropdown__group');
+            const dropdown = el.querySelector('.inline-to-dropdown__menu');
+            const dropdownChildren = dropdown.querySelectorAll('.inline-to-dropdown__item');
 
-            dropdownChildren.forEach((item) => {
+            for (const item of dropdownChildren) {
                 container.appendChild(item);
-            });
+            }
             
             while (isOverflown(container) === true) {
-                dropdownChildItem = container.querySelectorAll('.inline-to-dropdown__item');
-                lastDropdownChildItem = dropdownChildItem[dropdownChildItem.length - 1];
+                const dropdownChildItem = container.querySelectorAll('.inline-to-dropdown__item');
+                const lastDropdownChildItem = dropdownChildItem[dropdownChildItem.length - 1];
                 dropdown.prepend(lastDropdownChildItem);
-            }
-
-            if (dropdown.children.length > 0) {
-                el.classList.add('js-dropdown-has-children');
-            } else {
-                el.classList.remove('js-dropdown-has-children');
             }
         }
 
         function isOverflown(element) {
-            return element.scrollWidth > element.clientWidth;
+            return Math.floor(element.scrollWidth) > Math.floor(element.clientWidth);
         }
 
-        function debounce(func) {
+        function itdDebounce(func) {
             let timer;
-
-            return function(event){
+            return function(event) {
                 if (timer) clearTimeout(timer);
                 timer = setTimeout(func, 200, event);
             };
